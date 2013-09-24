b71279687240411b1aa2e5711c44d494a2c317d7
Rafael Oliveira <ludug3r0@gmail.com>
2013-09-24 16:13:50 -0300

    setting foreign keys (non-constraints)

--- db/create_table.sql
+++ db/create_table.sql
@@ -2,7 +2,7 @@ DROP DATABASE prf;
 CREATE DATABASE prf;
 USE prf;
 
-CREATE TABLE veiculo (
+CREATE TABLE veiculo ( -- cadastro dos dados do veículo
   veiid INT AUTO_INCREMENT PRIMARY KEY,
   veiano CHAR(4),
   veitmvcodigo INT,
@@ -23,10 +23,10 @@ CREATE TABLE veiculo (
   veisequencial INT
 );
 
-CREATE TABLE pessoa (
+CREATE TABLE pessoa ( -- cadatro da pessoa envolvida na ocorrência
   pesid INT AUTO_INCREMENT PRIMARY KEY,
   pesexpedidor CHAR(10),
-  pesufexpedidora CHAR(2),
+  pesufexpedidora CHAR(2), 
   pesnaturalidade CHAR(5),
   pesnacionalidade INT,
   pessexo CHAR(1),
@@ -38,7 +38,7 @@ CREATE TABLE pessoa (
   pespaisori INT,
   pesmunicipiodest CHAR(5),
   pespaisdest INT,
-  pesveiid INT, -- foreign key (veiculo)
+  pesveiid INT, -- FK(veiculo)
   pesestadofisico SMALLINT,
   pescinto char(1),
   pescapacete CHAR(1),
@@ -60,7 +60,9 @@ CREATE TABLE pessoa (
   pestclcodigo INT,
   pesoenid INT
 );
-CREATE TABLE ocorrencia (
+ALTER TABLE pessoa ADD FOREIGN KEY (pesveiid) REFERENCES veiculo(veiid);
+
+CREATE TABLE ocorrencia ( -- registro de ocorrência confirmada a partir das comunicações recebidas
   ocoid INT AUTO_INCREMENT PRIMARY KEY, 
   ocolocal INT, 
   ocostatus CHAR(1), 
@@ -76,33 +78,33 @@ CREATE TABLE ocorrencia (
   ocoresolucao_monta CHAR(10)
 );
 
-CREATE TABLE tipoenvolvido (
+CREATE TABLE tipoenvolvido ( -- Tabela que qualifica os envolvidos em uma ocorrência de crime
   ttecodigo INT AUTO_INCREMENT PRIMARY KEY,
   tteatualiza CHAR(1),
   tteativo CHAR(1)	
 );
 
-CREATE TABLE tipoacidente (
+CREATE TABLE tipoacidente ( -- Qualifica os tipos de acidente
   ttacodigo INT AUTO_INCREMENT PRIMARY KEY,
   ttaatualiza CHAR(1),
   ttarelacidente CHAR(1), 
   ttaativo CHAR(1)	
 );
 
-CREATE TABLE tipoveiculo (
+CREATE TABLE tipoveiculo ( -- Identifica os tipos de veículos 
   tvvcodigo INT AUTO_INCREMENT PRIMARY KEY,
   tvvatualiza CHAR(1),
   tvvrelacidente CHAR(1),
   tvvativo CHAR(1)		
 );
 
-CREATE TABLE municipio (
+CREATE TABLE municipio ( -- Cadastro do município da ocorrência.
   tmucodigo CHAR(5) PRIMARY KEY,
   tmuuf CHAR(2),
   tmuatualiza CHAR(1)
 );
 
-CREATE TABLE localbr (
+CREATE TABLE localbr ( -- Armazena o local da BR onde aconteceu a ocorrência. 
   lbrid INT AUTO_INCREMENT PRIMARY KEY, 
   lbruf CHAR(2), 
   lbrbr CHAR(3), 
@@ -113,51 +115,51 @@ CREATE TABLE localbr (
   lbratualiza CHAR(1)
 );
 
-CREATE TABLE uf (
+CREATE TABLE uf ( -- Cadastro da UF onde foi feito a ocorrência. 
   tufuf CHAR(2) PRIMARY KEY, 
   tufdenominacao VARCHAR(60)
 );
 
-CREATE TABLE tipoareaespecial (
+CREATE TABLE tipoareaespecial ( -- Identifica os tipo de área especiais que existem 
   taecodigo INT AUTO_INCREMENT PRIMARY KEY, 
   taeatualiza CHAR(1)
 );
 
-CREATE TABLE tipoapreensao (
+CREATE TABLE tipoapreensao ( -- Identifica os tipos de apreensão 
   ttpcodigo INT AUTO_INCREMENT PRIMARY KEY, 
   ttpatualiza CHAR(1)
 );
 
-CREATE TABLE tipodetencao (
+CREATE TABLE tipodetencao ( -- Cadastro dos tipos possíveis de detenção
   ttdcodigo INT AUTO_INCREMENT PRIMARY KEY, 
   ttdatualiza CHAR(1),
   ttdrelacidente CHAR(1)
 );
 
-CREATE TABLE tiporeceptor (
+CREATE TABLE tiporeceptor ( -- Identifica os tipos de receptores em uma ocorrência encaminhada 
 	ttrcodigo INT AUTO_INCREMENT PRIMARY KEY,
 	ttratualiza CHAR(1), 
 	ttrdelegacia CHAR(1)
 );
 
-CREATE TABLE tipodocumento (
+CREATE TABLE tipodocumento ( -- Cadastro de tipos de documentos
   ttocodigo INT AUTO_INCREMENT PRIMARY KEY,
   ttoatualiza CHAR(1), 
   ttorelapreensao CHAR(1),
   ttorelrecuperacao CHAR(1)
 );
 
-CREATE TABLE tipounidadeoperacional (
+CREATE TABLE tipounidadeoperacional ( -- Qualificação dos tipos das unidades operacionais 
 	ttucodigo INT AUTO_INCREMENT PRIMARY KEY, 
 	ttuatualiza CHAR(1)
 );
 
-CREATE TABLE unidadeoperacional (
+CREATE TABLE unidadeoperacional ( -- Qualificação dos tipos das unidades operacionais 
   uniid INT AUTO_INCREMENT PRIMARY KEY, 
   uniunidade CHAR(6), 
   unilotacao CHAR(9), 
   unisigla CHAR(15), 
-  unittucodigo INT, -- foreign key (tipounidadeoperacional)
+  unittucodigo INT, -- FK(tipounidadeoperacional)
   uniunidaderesponsavel INT, 
   unidenominacao VARCHAR(80), 
   unimunicipio CHAR(5), 
@@ -165,28 +167,33 @@ CREATE TABLE unidadeoperacional (
   unilongitude CHAR(10), 
   unihelicoptero CHAR(1)
 );
+ALTER TABLE unidadeoperacional ADD FOREIGN KEY (unittucodigo) REFERENCES tipounidadeoperacional(ttucodigo);
 
-CREATE TABLE ocorrenciaveiculo (
+CREATE TABLE ocorrenciaveiculo ( -- Cadastro da ocorrência referente ao veículo da pessoa envolvida
   ocvid INT AUTO_INCREMENT PRIMARY KEY, 
-  ocvocoid INT, -- foreign key (ocorrencia)
-  ocvveiid INT -- foreign key (veiculo)
-
+  ocvocoid INT, -- FK(ocorrencia)
+  ocvveiid INT -- FK(veiculo)
 );
+ALTER TABLE ocorrenciaveiculo ADD FOREIGN KEY (ocvocoid) REFERENCES ocorrencia(ocoid);
+ALTER TABLE ocorrenciaveiculo ADD FOREIGN KEY (ocvveiid) REFERENCES veiculo(veiid);
 
-CREATE TABLE ocorrenciapessoa (
+CREATE TABLE ocorrenciapessoa ( -- Cadastro de pessoas envolvidas no acidente.
   opeid INT AUTO_INCREMENT PRIMARY KEY, 
-  opeocoid INT, -- foreign key (ocorrencia)
-  opepesid INT, -- foreign key (pessoa)
+  opeocoid INT, -- FK(ocorrencia)
+  opepesid INT, -- FK(pessoa)
   opeportevalidade DATE, 
-  opettecodigo INT, -- foreign key (tipoenvolvido)
+  opettecodigo INT, -- FK(tipoenvolvido)
   opeestrangeiro CHAR(1), 
   opeanexo CHAR(1), 
   opecondalegadas CHAR(1)
 );
+ALTER TABLE ocorrenciapessoa ADD FOREIGN KEY (opeocoid) REFERENCES ocorrencia(ocoid);
+ALTER TABLE ocorrenciapessoa ADD FOREIGN KEY (opepesid) REFERENCES pessoa(pesid);
+ALTER TABLE ocorrenciapessoa ADD FOREIGN KEY (opettecodigo) REFERENCES tipoenvolvido(ttecodigo);
 
-CREATE TABLE ocorrenciaacidente (
-  oacocoid INT PRIMARY KEY, -- foreign key (ocorrencia)
-  oacttacodigo INT, -- foreign key (tipoacidente)
+CREATE TABLE ocorrenciaacidente ( -- Cadastro de ocorrência envolvendo veículos. 
+  oacocoid INT PRIMARY KEY, -- FK(ocorrencia)
+  oacttacodigo INT, -- FK(tipoacidente)
   oactcacodigo INT, 
   oacdano CHAR(1), 
   oacdanoterc CHAR(1), 
@@ -217,3 +224,5 @@ CREATE TABLE ocorrenciaacidente (
   oacversaocroqui CHAR(1), 
   oacsitio INT
 );
+ALTER TABLE ocorrenciaacidente ADD FOREIGN KEY (oacocoid) REFERENCES ocorrencia(ocoid);
+ALTER TABLE ocorrenciaacidente ADD FOREIGN KEY (oacttacodigo) REFERENCES tipoacidente(ttacodigo);
