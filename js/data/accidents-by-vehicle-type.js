4d039ef340199f9d6a2bc21f851db1b741a27562
Gislene Pereira <gislene01@gmail.com>
2013-10-07 18:16:12 -0300

    Adicionado json de acidentes por tipo de veículo

---
+++ js/data/accidents-by-vehicle-type.js
@@ -0,0 +1,677 @@
+[
+	{
+		'uf' : AC,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 2254
+	},
+	{
+		'uf' : AC,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 141
+	},
+	{
+		'uf' : AC,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 629
+	},
+	{
+		'uf' : AC,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 989
+	},
+	{
+		'uf' : AC,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 118
+	},
+	{
+		'uf' : AL,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 12147
+	},
+	{
+		'uf' : AL,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 417
+	},
+	{
+		'uf' : AL,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 4466
+	},
+	{
+		'uf' : AL,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 2561
+	},
+	{
+		'uf' : AL,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 1195
+	},
+	{
+		'uf' : AM,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 1357
+	},
+	{
+		'uf' : AM,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 5
+	},
+	{
+		'uf' : AM,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 315
+	},
+	{
+		'uf' : AM,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 120
+	},
+	{
+		'uf' : AM,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 108
+	},
+	{
+		'uf' : AP,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 962
+	},
+	{
+		'uf' : AP,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 115
+	},
+	{
+		'uf' : AP,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 189
+	},
+	{
+		'uf' : AP,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 387
+	},
+	{
+		'uf' : AP,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 58
+	},
+	{
+		'uf' : BA,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 48178
+	},
+	{
+		'uf' : BA,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 990
+	},
+	{
+		'uf' : BA,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 26738
+	},
+	{
+		'uf' : BA,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 7806
+	},
+	{
+		'uf' : BA,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 4031
+	},
+	{
+		'uf' : CE,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 18427
+	},
+	{
+		'uf' : CE,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 931
+	},
+	{
+		'uf' : CE,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 6612
+	},
+	{
+		'uf' : CE,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 5026
+	},
+	{
+		'uf' : CE,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 1747
+	},
+	{
+		'uf' : DF,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 10598
+	},
+	{
+		'uf' : DF,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 97
+	},
+	{
+		'uf' : DF,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 1195
+	},
+	{
+		'uf' : DF,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 1695
+	},
+	{
+		'uf' : DF,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 552
+	},
+	{
+		'uf' : ES,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 47689
+	},
+	{
+		'uf' : ES,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 937
+	},
+	{
+		'uf' : ES,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 19950
+	},
+	{
+		'uf' : ES,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 8056
+	},
+	{
+		'uf' : ES,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 3305
+	},
+	{
+		'uf' : GO,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 43703
+	},
+	{
+		'uf' : GO,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 625
+	},
+	{
+		'uf' : GO,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 17323
+	},
+	{
+		'uf' : GO,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 7327
+	},
+	{
+		'uf' : GO,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 1928
+	},
+	{
+		'uf' : MA,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 13559
+	},
+	{
+		'uf' : MA,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 550
+	},
+	{
+		'uf' : MA,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 6900
+	},
+	{
+		'uf' : MA,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 3578
+	},
+	{
+		'uf' : MA,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 1673
+	},
+	{
+		'uf' : MG,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 138700
+	},
+	{
+		'uf' : MG,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 2042
+	},
+	{
+		'uf' : MG,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 73445
+	},
+	{
+		'uf' : MG,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 18068
+	},
+	{
+		'uf' : MG,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 7870
+	},
+	{
+		'uf' : MS,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 14830
+	},
+	{
+		'uf' : MS,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 419
+	},
+	{
+		'uf' : MS,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 8974
+	},
+	{
+		'uf' : MS,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 2963
+	},
+	{
+		'uf' : MS,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 706
+	},
+	{
+		'uf' : MT,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 13271
+	},
+	{
+		'uf' : MT,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 328
+	},
+	{
+		'uf' : MT,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 16932
+	},
+	{
+		'uf' : MT,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 3216
+	},
+	{
+		'uf' : MT,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 805
+	},
+	{
+		'uf' : PA,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 23174
+	},
+	{
+		'uf' : PA,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 1007
+	},
+	{
+		'uf' : PA,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 8818
+	},
+	{
+		'uf' : PA,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 5362
+	},
+	{
+		'uf' : PA,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 4625
+	},
+	{
+		'uf' : PB,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 24927
+	},
+	{
+		'uf' : PB,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 630
+	},
+	{
+		'uf' : PB,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 4365
+	},
+	{
+		'uf' : PB,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 5566
+	},
+	{
+		'uf' : PB,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 851
+	},
+	{
+		'uf' : PE,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 41068
+	},
+	{
+		'uf' : PE,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 751
+	},
+	{
+		'uf' : PE,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 13934
+	},
+	{
+		'uf' : PE,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 9322
+	},
+	{
+		'uf' : PE,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 3280
+	},
+	{
+		'uf' : PI,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 13550
+	},
+	{
+		'uf' : PI,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 644
+	},
+	{
+		'uf' : PI,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 4219
+	},
+	{
+		'uf' : PI,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 4783
+	},
+	{
+		'uf' : PI,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 1196
+	},
+	{
+		'uf' : PR,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 104916
+	},
+	{
+		'uf' : PR,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 1757
+	},
+	{
+		'uf' : PR,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 37812
+	},
+	{
+		'uf' : PR,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 17098
+	},
+	{
+		'uf' : PR,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 3885
+	},
+	{
+		'uf' : RJ,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 105663
+	},
+	{
+		'uf' : RJ,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 1221
+	},
+	{
+		'uf' : RJ,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 37421
+	},
+	{
+		'uf' : RJ,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 9099
+	},
+	{
+		'uf' : RJ,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 10001
+	},
+	{
+		'uf' : RN,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 27135
+	},
+	{
+		'uf' : RN,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 547
+	},
+	{
+		'uf' : RN,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 4162
+	},
+	{
+		'uf' : RN,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 5567
+	},
+	{
+		'uf' : RN,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 2099
+	},
+	{
+		'uf' : RO,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 17075
+	},
+	{
+		'uf' : RO,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 871
+	},
+	{
+		'uf' : RO,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 6967
+	},
+	{
+		'uf' : RO,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 7054
+	},
+	{
+		'uf' : RO,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 981
+	},
+	{
+		'uf' : RR,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 1725
+	},
+	{
+		'uf' : RR,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 145
+	},
+	{
+		'uf' : RR,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 241
+	},
+	{
+		'uf' : RR,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 801
+	},
+	{
+		'uf' : RR,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 65
+	},
+	{
+		'uf' : RS,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 88262
+	},
+	{
+		'uf' : RS,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 1214
+	},
+	{
+		'uf' : RS,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 25450
+	},
+	{
+		'uf' : RS,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 13949
+	},
+	{
+		'uf' : RS,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 3666
+	},
+	{
+		'uf' : SC,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 124287
+	},
+	{
+		'uf' : SC,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 1935
+	},
+	{
+		'uf' : SC,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 37328
+	},
+	{
+		'uf' : SC,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 22822
+	},
+	{
+		'uf' : SC,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 3351
+	},
+	{
+		'uf' : SE,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 8317
+	},
+	{
+		'uf' : SE,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 160
+	},
+	{
+		'uf' : SE,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 3715
+	},
+	{
+		'uf' : SE,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 1514
+	},
+	{
+		'uf' : SE,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 848
+	},
+	{
+		'uf' : SP,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 75795
+	},
+	{
+		'uf' : SP,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 1063
+	},
+	{
+		'uf' : SP,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 39208
+	},
+	{
+		'uf' : SP,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 10592
+	},
+	{
+		'uf' : SP,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 4190
+	},
+	{
+		'uf' : TO,
+		'tipo_veiculo' : 'Automovel',
+		'qtd_ocorrencias' : 3901
+	},
+	{
+		'uf' : TO,
+		'tipo_veiculo' : 'Bicicleta',
+		'qtd_ocorrencias' : 198
+	},
+	{
+		'uf' : TO,
+		'tipo_veiculo' : 'Caminhao',
+		'qtd_ocorrencias' : 3495
+	},
+	{
+		'uf' : TO,
+		'tipo_veiculo' : 'Motocicleta',
+		'qtd_ocorrencias' : 1177
+	},
+	{
+		'uf' : TO,
+		'tipo_veiculo' : 'Onibus',
+		'qtd_ocorrencias' : 312
+	}
+]
