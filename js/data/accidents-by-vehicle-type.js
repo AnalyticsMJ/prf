e8d599447ef31110d666dc1a161f3f61af856d95
Johann Arispe <johann.arispe@gmail.com>
2013-10-09 15:53:32 -0300

    layout/dadosVeiculo

--- js/data/accidents-by-vehicle-type.js
+++ js/data/accidents-by-vehicle-type.js
@@ -1,13 +1,220 @@
 define(function() {
-	return [
-		{
-			'uf' : 'AC',
-			'tipo_veiculo' : 'Automovel',
-			'qtd_ocorrencias_bicicleta' : 1241,
-			'qtd_ocorrencias_automovel' : 3141,
-			'qtd_ocorrencias_caminhao' : 341,
-			'qtd_ocorrencias_motocicleta' : 1341,
-			'qtd_ocorrencias_onibus' : 1141
-		},
-	];
+	return[
+	{
+		'uf' : 'AC',
+		'qtd_ocorrencias_bicicleta' : 141,
+		'qtd_ocorrencias_motocicleta' : 989,
+		'qtd_ocorrencias_automovel' : 2254,
+		'qtd_ocorrencias_caminhao' : 629,
+		'qtd_ocorrencias_onibus' : 118
+	},
+	{
+		'uf' : 'AL',
+		'qtd_ocorrencias_bicicleta' : 417,
+		'qtd_ocorrencias_motocicleta' : 2561,
+		'qtd_ocorrencias_automovel' : 12147,
+		'qtd_ocorrencias_caminhao' : 4466,
+		'qtd_ocorrencias_onibus' : 1195
+	},
+	{
+		'uf' : 'AM',
+		'qtd_ocorrencias_bicicleta' : 5,
+		'qtd_ocorrencias_motocicleta' : 120,
+		'qtd_ocorrencias_automovel' : 1357,
+		'qtd_ocorrencias_caminhao' : 315,
+		'qtd_ocorrencias_onibus' : 108
+	},
+	{
+		'uf' : 'AP',
+		'qtd_ocorrencias_bicicleta' : 115,
+		'qtd_ocorrencias_motocicleta' : 387,
+		'qtd_ocorrencias_automovel' : 962,
+		'qtd_ocorrencias_caminhao' : 189,
+		'qtd_ocorrencias_onibus' : 58
+	},
+	{
+		'uf' : 'BA',
+		'qtd_ocorrencias_bicicleta' : 990,
+		'qtd_ocorrencias_motocicleta' : 7806,
+		'qtd_ocorrencias_automovel' : 48178,
+		'qtd_ocorrencias_caminhao' : 26738,
+		'qtd_ocorrencias_onibus' : 4031
+	},
+	{
+		'uf' : 'CE',
+		'qtd_ocorrencias_bicicleta' : 931,
+		'qtd_ocorrencias_motocicleta' : 5026,
+		'qtd_ocorrencias_automovel' : 18427,
+		'qtd_ocorrencias_caminhao' : 6612,
+		'qtd_ocorrencias_onibus' : 1747
+	},
+	{
+		'uf' : 'DF',
+		'qtd_ocorrencias_bicicleta' : 97,
+		'qtd_ocorrencias_motocicleta' : 1695,
+		'qtd_ocorrencias_automovel' : 10598,
+		'qtd_ocorrencias_caminhao' : 1195,
+		'qtd_ocorrencias_onibus' : 552
+	},
+	{
+		'uf' : 'ES',
+		'qtd_ocorrencias_bicicleta' : 937,
+		'qtd_ocorrencias_motocicleta' : 8056,
+		'qtd_ocorrencias_automovel' : 47689,
+		'qtd_ocorrencias_caminhao' : 19950,
+		'qtd_ocorrencias_onibus' : 3305
+	},
+	{
+		'uf' : 'GO',
+		'qtd_ocorrencias_bicicleta' : 625,
+		'qtd_ocorrencias_motocicleta' : 7327,
+		'qtd_ocorrencias_automovel' : 43703,
+		'qtd_ocorrencias_caminhao' : 17323,
+		'qtd_ocorrencias_onibus' : 1928
+	},
+	{
+		'uf' : 'MA',
+		'qtd_ocorrencias_bicicleta' : 550,
+		'qtd_ocorrencias_motocicleta' : 3578,
+		'qtd_ocorrencias_automovel' : 13559,
+		'qtd_ocorrencias_caminhao' : 6900,
+		'qtd_ocorrencias_onibus' : 1673
+	},
+	{
+		'uf' : 'MG',
+		'qtd_ocorrencias_bicicleta' : 2042,
+		'qtd_ocorrencias_motocicleta' : 18068,
+		'qtd_ocorrencias_automovel' : 138700,
+		'qtd_ocorrencias_caminhao' : 73445,
+		'qtd_ocorrencias_onibus' : 7870
+	},
+	{
+		'uf' : 'MS',
+		'qtd_ocorrencias_bicicleta' : 419,
+		'qtd_ocorrencias_motocicleta' : 2963,
+		'qtd_ocorrencias_automovel' : 14830,
+		'qtd_ocorrencias_caminhao' : 8974,
+		'qtd_ocorrencias_onibus' : 706
+	},
+	{
+		'uf' : 'MT',
+		'qtd_ocorrencias_bicicleta' : 328,
+		'qtd_ocorrencias_motocicleta' : 3216,
+		'qtd_ocorrencias_automovel' : 13271,
+		'qtd_ocorrencias_caminhao' : 16932,
+		'qtd_ocorrencias_onibus' : 805
+	},
+	{
+		'uf' : 'PA',
+		'qtd_ocorrencias_bicicleta' : 1007,
+		'qtd_ocorrencias_motocicleta' : 5362,
+		'qtd_ocorrencias_automovel' : 23174,
+		'qtd_ocorrencias_caminhao' : 8818,
+		'qtd_ocorrencias_onibus' : 4625
+	},
+	{
+		'uf' : 'PB',
+		'qtd_ocorrencias_bicicleta' : 630,
+		'qtd_ocorrencias_motocicleta' : 5566,
+		'qtd_ocorrencias_automovel' : 24927,
+		'qtd_ocorrencias_caminhao' : 4365,
+		'qtd_ocorrencias_onibus' : 851
+	},
+	{
+		'uf' : 'PE',
+		'qtd_ocorrencias_bicicleta' : 751,
+		'qtd_ocorrencias_motocicleta' : 9322,
+		'qtd_ocorrencias_automovel' : 41068,
+		'qtd_ocorrencias_caminhao' : 13934,
+		'qtd_ocorrencias_onibus' : 3280
+	},
+	{
+		'uf' : 'PI',
+		'qtd_ocorrencias_bicicleta' : 644,
+		'qtd_ocorrencias_motocicleta' : 4783,
+		'qtd_ocorrencias_automovel' : 13550,
+		'qtd_ocorrencias_caminhao' : 4219,
+		'qtd_ocorrencias_onibus' : 1196
+	},
+	{
+		'uf' : 'PR',
+		'qtd_ocorrencias_bicicleta' : 1757,
+		'qtd_ocorrencias_motocicleta' : 17098,
+		'qtd_ocorrencias_automovel' : 104916,
+		'qtd_ocorrencias_caminhao' : 37812,
+		'qtd_ocorrencias_onibus' : 3885
+	},
+	{
+		'uf' : 'RJ',
+		'qtd_ocorrencias_bicicleta' : 1221,
+		'qtd_ocorrencias_motocicleta' : 9099,
+		'qtd_ocorrencias_automovel' : 105663,
+		'qtd_ocorrencias_caminhao' : 37421,
+		'qtd_ocorrencias_onibus' : 10001
+	},
+	{
+		'uf' : 'RN',
+		'qtd_ocorrencias_bicicleta' : 547,
+		'qtd_ocorrencias_motocicleta' : 5567,
+		'qtd_ocorrencias_automovel' : 27135,
+		'qtd_ocorrencias_caminhao' : 4162,
+		'qtd_ocorrencias_onibus' : 2099
+	},
+	{
+		'uf' : 'RO',
+		'qtd_ocorrencias_bicicleta' : 871,
+		'qtd_ocorrencias_motocicleta' : 7054,
+		'qtd_ocorrencias_automovel' : 17075,
+		'qtd_ocorrencias_caminhao' : 6967,
+		'qtd_ocorrencias_onibus' : 981
+	},
+	{
+		'uf' : 'RR',
+		'qtd_ocorrencias_bicicleta' : 145,
+		'qtd_ocorrencias_motocicleta' : 801,
+		'qtd_ocorrencias_automovel' : 1725,
+		'qtd_ocorrencias_caminhao' : 241,
+		'qtd_ocorrencias_onibus' : 65
+	},
+	{
+		'uf' : 'RS',
+		'qtd_ocorrencias_bicicleta' : 1214,
+		'qtd_ocorrencias_motocicleta' : 13949,
+		'qtd_ocorrencias_automovel' : 88262,
+		'qtd_ocorrencias_caminhao' : 25450,
+		'qtd_ocorrencias_onibus' : 3666
+	},
+	{
+		'uf' : 'SC',
+		'qtd_ocorrencias_bicicleta' : 1935,
+		'qtd_ocorrencias_motocicleta' : 22822,
+		'qtd_ocorrencias_automovel' : 124287,
+		'qtd_ocorrencias_caminhao' : 37328,
+		'qtd_ocorrencias_onibus' : 3351
+	},
+	{
+		'uf' : 'SE',
+		'qtd_ocorrencias_bicicleta' : 160,
+		'qtd_ocorrencias_motocicleta' : 1514,
+		'qtd_ocorrencias_automovel' : 8317,
+		'qtd_ocorrencias_caminhao' : 3715,
+		'qtd_ocorrencias_onibus' : 848
+	},
+	{
+		'uf' : 'SP',
+		'qtd_ocorrencias_bicicleta' : 1063,
+		'qtd_ocorrencias_motocicleta' : 10592,
+		'qtd_ocorrencias_automovel' : 75795,
+		'qtd_ocorrencias_caminhao' : 39208,
+		'qtd_ocorrencias_onibus' : 4190
+	},
+	{
+		'uf' : 'TO',
+		'qtd_ocorrencias_bicicleta' : 198,
+		'qtd_ocorrencias_motocicleta' : 1177,
+		'qtd_ocorrencias_automovel' : 3901,
+		'qtd_ocorrencias_caminhao' : 3495,
+		'qtd_ocorrencias_onibus' : 312
+	}
+]
 });
