6765cc14507256ec3357b49f7256358c6bc9463a
Gislene Pereira <gislene01@gmail.com>
2013-10-09 15:38:44 -0300

    Alterada query de ocorrências por tipo de veiculo

--- db/queries/ocorrencias_por_estado_tipo_veiculo.sql
+++ db/queries/ocorrencias_por_estado_tipo_veiculo.sql
@@ -1,21 +1,25 @@
 select 
-	tmuuf uf
-	, case  
-		when tvvcodigo = 1 then 'Bicicleta'
-		when tvvcodigo in (3,4) then 'Motocicleta'
-		when tvvcodigo in (7,16,17,23) then 'Automovel'
-		when tvvcodigo in (13,18,25) then 'Caminhao'
-		when tvvcodigo in (8,9) then 'Onibus'
-		else ''
-		end as 'tipo_veiculo'
-	, count(ocvid) 'qtd_ocorrencias'
-from ocorrenciaveiculo
-join ocorrencia on ocoid = ocvocoid
-join veiculo on veiid = ocvveiid
-join tipoveiculo on tvvcodigo = veitvvcodigo
-join municipio on tmucodigo = ocomunicipio
-where tvvcodigo in (1,3,4,7,16,17,23,13,18,25,8,9)
-group by tmuuf, tipo_veiculo
-
-
+	stat.uf
+	,sum(stat.oco_bicicleta) qtd_ocorrencias_bicicleta
+	,sum(stat.oco_motocicleta) qtd_ocorrencias_motocicleta 
+	,sum(stat.oco_automovel) qtd_ocorrencias_automovel 
+	,sum(stat.oco_caminhao) qtd_ocorrencias_caminhao 
+	,sum(stat.oco_onibus) qtd_ocorrencias_onibus 
+from (
+	select 
+		tmuuf uf
+		, case when tvvcodigo = 1 then count(1) end as 'oco_bicicleta'
+		, case when tvvcodigo in (3,4) then count(1) end as 'oco_motocicleta'
+		, case when tvvcodigo in (7,16,17,23) then count(1) end as 'oco_automovel'
+		, case when tvvcodigo in (13,18,25) then count(1) end as 'oco_caminhao'
+		, case when tvvcodigo in (8,9) then count(1) end as 'oco_onibus'
+	from ocorrenciaveiculo
+	join ocorrencia on ocoid = ocvocoid
+	join veiculo on veiid = ocvveiid
+	join tipoveiculo on tvvcodigo = veitvvcodigo
+	join municipio on tmucodigo = ocomunicipio
+	where tvvcodigo in (1,3,4,7,16,17,23,13,18,25,8,9)
+	group by tmuuf, tvvcodigo
+) as stat
+group by uf
 
