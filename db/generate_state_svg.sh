279a954a7fef705a720dc6e78706b09e9fddd54a
Ricardo Cavalcanti <rcavalca@thoughtworks.com>
2013-10-15 17:18:05 -0300

    possibilitando geracao de um ou varios estados

--- db/generate_state_svg.sh
+++ db/generate_state_svg.sh
@@ -2,8 +2,13 @@
 
 CURRENT_DIR=`pwd`
 
-ESTADOS="AC AL AP AM BA CE DF ES GO MA MT MS MG PA PB PR PE PI RJ RN RS RO RR SC SP SE TO"
+ESTADOS=$1
+default_estados="AC AL AP AM BA CE DF ES GO MA MT MS MG PA PB PR PE PI RJ RN RS RO RR SC SP SE TO"
 
+
+${ESTADOS:=$default_estados}
+
+echo $ESTADOS
 DATA_DIR="../data" 
 
 OUTPUT_DIR="../temp"
@@ -20,9 +25,8 @@ cd $DATA_DIR
 mkdir "${OUTPUT_DIR}/output"
 for UF in $ESTADOS; do
   	echo "Gerando svg para ${UF}..."
-	kartograph "${OUTPUT_DIR}/config/${UF}.json" -o "${OUTPUT_DIR}/output/${UF}.svg" -s style.css
+    kartograph "${OUTPUT_DIR}/config/${UF}.json" -o "${OUTPUT_DIR}/output/${UF}.svg" -s style.css
 	kartograph "${OUTPUT_DIR}/config/thumb-${UF}.json" -o "${OUTPUT_DIR}/output/thumb-${UF}.svg" -s thumb-style.css
-	
 done
 
 cd $CURRENT_DIR
