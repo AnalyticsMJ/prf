6cf00e5ba7d4648e0ed708c49e5595e368995957
Paolo Carrasco <pcarras@PaoloCarrasco.local>
2013-10-16 16:55:41 -0300

    moved the styles of the maps to a separated css file

--- js/app/views/main-view.js
+++ js/app/views/main-view.js
@@ -25,23 +25,8 @@ define(['ko', 'underscore', 'app/views/state-view', 'data/data-finder','kartogra
         
       map.loadMap('img/estados/'+state.abbreviation +'.svg', function() {
 
-
-        map.addLayer('vizinhos', {
-            styles: {
-                      stroke: '##F3F3F3',
-                      fill: '#E6E6E6',
-                      'stroke-width': 2,
-                      'stroke-linejoin': 'round'
-                  }
-          });
-        map.addLayer('estado', {
-            styles: {
-                      stroke: '#ada',
-                      fill: '#cc4',
-                      'stroke-width': 4,
-                      'stroke-linejoin': 'round'
-                  }
-          });
+        map.addLayer('vizinhos');
+        map.addLayer('estado');
         map.addLayer('rodovias');
         
         var colorscale = new chroma.scale('Reds').domain([0,1,2,3,4,5]);
@@ -50,12 +35,6 @@ define(['ko', 'underscore', 'app/views/state-view', 'data/data-finder','kartogra
             return colorscale(data.categoria);
         });
 
-
-
-
-        map.addLayer('estado');
-        map.addLayer('vizinhos');
-        
       });
     });
   };
