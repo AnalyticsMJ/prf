3a7c80d5be92d38beb703eb4c1e521b73d7d122d
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-10-16 21:55:20 -0300

    refactoring the code

--- js/app/views/main-view.js
+++ js/app/views/main-view.js
@@ -6,8 +6,6 @@ define(['ko', 'underscore', 'app/views/state-view', 'data/data-finder','kartogra
   self.states = ko.observableArray();
   self.selectedState = ko.observable(null);
 
-  console.log('ha');
-  
   self.formatNumber = function(value) {
       return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   };
@@ -39,71 +37,64 @@ define(['ko', 'underscore', 'app/views/state-view', 'data/data-finder','kartogra
     });
   };
     
-    
-    showSeverityChart = function(stats) {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Gravidade');
-        data.addColumn('number', 'Acidentes'); 
-        data.addRows(_.map(stats, function(d) {
-          return [d.gravidade, d.qtd_ocorrencia]
-        }));
-        var options = { 
-              colors: ['#EFA190','#EF846E','#DF5A3E','#912A14'],
-              //colors: ['#FCAE91','#FB6A4A','#DE2D26','#A50F15'],
-              width: '100%', height: '100%',
-              animation: { duration: 1000 },
-              backgroundColor: 'transparent',
-              chartArea:{width: '90%', height: '90%',},
-               legend: { position: 'none'}                 
-              };
-            
-         var chart = new google.visualization.PieChart(document.getElementById('severity')); 
-         chart.draw(data, options);
-    }
+  self.loadStates = function(states) {
+    selectedStateView(states);
+  };
 
-
-       
-    showByHourChart = function(stats) {
-
+  showSeverityChart = function(stats) {
       var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Hora');
-      data.addColumn('number', 'Acidentes');
-      data.addColumn({type: 'string', role: 'tooltip'});
+      data.addColumn('string', 'Gravidade');
+      data.addColumn('number', 'Acidentes'); 
       data.addRows(_.map(stats, function(d) {
-        return [d.hora, d.qtd_ocorrencia, d.hora+"h: "+d.qtd_ocorrencia+" acidentes"]
+        return [d.gravidade, d.qtd_ocorrencia]
       }));
       var options = { 
-            colors: ['#DF5A3E'],
+            colors: ['#EFA190','#EF846E','#DF5A3E','#912A14'],
             width: '100%', height: '100%',
             animation: { duration: 1000 },
             backgroundColor: 'transparent',
-            chartArea:{width: '80%', height: '80%',},
-            curveType: 'function',
-            hAxis: { 
-                  baselineColor: 'transparent',
-                ticks: [0,6,12,18,23], 
-                gridlines: { count: 4, color: 'transparent'}
-            },
-            vAxis:{ 
-               
-                textPosition :'none',
-                 gridlines: { color: 'transparent'}
-            },
+            chartArea:{width: '90%', height: '90%',},
              legend: { position: 'none'}                 
             };
-            
-        var chart = new google.visualization.ColumnChart(document.getElementById('hour'));
-  
-        chart.draw(data, options);    
-
-    }
+          
+       var chart = new google.visualization.PieChart(document.getElementById('severity')); 
+       chart.draw(data, options);
+  }
      
-    
+  showByHourChart = function(stats) {
 
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Hora');
+    data.addColumn('number', 'Acidentes');
+    data.addColumn({type: 'string', role: 'tooltip'});
+    data.addRows(_.map(stats, function(d) {
+      return [d.hora, d.qtd_ocorrencia, d.hora+"h: "+d.qtd_ocorrencia+" acidentes"]
+    }));
+    var options = { 
+          colors: ['#DF5A3E'],
+          width: '100%', height: '100%',
+          animation: { duration: 1000 },
+          backgroundColor: 'transparent',
+          chartArea:{width: '80%', height: '80%',},
+          curveType: 'function',
+          hAxis: { 
+                baselineColor: 'transparent',
+              ticks: [0,6,12,18,23], 
+              gridlines: { count: 4, color: 'transparent'}
+          },
+          vAxis:{ 
+             
+              textPosition :'none',
+               gridlines: { color: 'transparent'}
+          },
+           legend: { position: 'none'}                 
+          };
+          
+      var chart = new google.visualization.ColumnChart(document.getElementById('hour'));
 
-    self.loadStates = function(states) {
-      selectedStateView(states);
-    };
+      chart.draw(data, options);    
 
-    return self;
-  });
+  }
+
+  return self;
+});
