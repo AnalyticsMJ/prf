ee0993e59d55dad7d3baef31eeb638cb5d4ad5da
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-10-17 15:43:28 -0300

    keeping selected the initial state, removing the cursor on selected states

--- js/main.js
+++ js/main.js
@@ -32,14 +32,29 @@ require.config({
 });
 
 require(['ko', 'app/views/main-view', 'data/data-finder', 'jquery'], function(ko, mainView, dataFinder, $) {
-    ko.applyBindings(mainView, document.getElementsByTagName('html')[0]);
-    
-    var states = dataFinder.allStates();
-    mainView.states(states);
-    mainView.stateClicked(states[0]);
-    var stateListElements = $('.statesList li');
-    stateListElements.on('click', 'a', function () {
-        stateListElements.find('.selected').removeClass('selected');
-        $(this).addClass('selected');
-    });
+    var init = (function () {
+        var loadStates = function() {
+            ko.applyBindings(mainView, document.getElementsByTagName('html')[0]);
+            var states = dataFinder.allStates();
+            mainView.states(states);
+            return states;
+        };
+
+        var presentPageSelecting = function(state) {
+            var stateListElements = $('.statesList li');
+            stateListElements.on('click', 'a', function () {
+                stateListElements.find('.selected').removeClass('selected');
+                $(this).addClass('selected');
+            });
+            stateListElements.find('a').first().click();
+            mainView.stateClicked(state);
+        };
+
+        return function(states) {
+            var states = loadStates();
+            presentPageSelecting(states[0]);
+        };
+    })();
+
+    init();
 });
