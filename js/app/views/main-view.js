740d6c77edfdfde32532781498b8bd9d300cf062
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-10-21 18:36:07 -0300

    sorting the stats by vehicle type

--- js/app/views/main-view.js
+++ js/app/views/main-view.js
@@ -35,7 +35,7 @@ define(['ko', 'app/views/map-render', 'app/views/chart-render'], function(ko, Ma
   }
 
   self.getVehicleTypeIcon = function (vehicleType) {
-    return 'img/bicycle-w100.png'; //iconsByVehicleType[vehicleType].icon;
+    return iconsByVehicleType[vehicleType].icon;
   } ;
 
   return self;
