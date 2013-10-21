7e238ee095439d825b5c6a817d3132a19a995662
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-10-21 00:44:21 -0200

    decoupling the rendering of charts from the main view

--- js/app/views/main-view.js
+++ js/app/views/main-view.js
@@ -1,5 +1,7 @@
-define(['ko', 'underscore', 'kartograph', 'qtip', 'app/views/map-render', 'goog!visualization,1,packages:[corechart]'], function(ko, _, $K, qtip, MapRender) {
-  var self = this, mapRender = new MapRender();
+define(['ko', 'app/views/map-render', 'app/views/chart-render'], function(ko, MapRender, ChartRender) {
+  var self = this,
+    mapRender = new MapRender(),
+    chartRender = new ChartRender();
 
   self.title = ko.observable("PRF");
   self.loaded = true;
@@ -15,63 +17,10 @@ define(['ko', 'underscore', 'kartograph', 'qtip', 'app/views/map-render', 'goog!
     $('.mapContainer > h3').fadeIn(500);
     $('#sideRight').fadeIn(1000);
     self.selectedState(state);
-    showSeverityChart(state.bySeverity);
-    showByHourChart(state.byHour);
+    chartRender.showSeverityChart(state.bySeverity);
+    chartRender.showByHourChart(state.byHour);
     mapRender.showMapOf(state);
   };
 
-  var showSeverityChart = function(stats) {
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Gravidade');
-      data.addColumn('number', 'Acidentes'); 
-      data.addRows(_.map(stats, function(d) {
-        return [d.gravidade, d.qtd_ocorrencia]
-      }));
-      var options = { 
-            colors: ['#EFA190','#EF846E','#DF5A3E','#912A14'],
-            width: '100%', height: '100%',
-            animation: { duration: 1000 },
-            backgroundColor: 'transparent',
-            chartArea:{width: '90%', height: '90%',},
-            legend: { position: 'none'}                 
-          };
-          
-       var chart = new google.visualization.PieChart(document.getElementById('severity')); 
-       chart.draw(data, options);
-  };
-     
-  var showByHourChart = function(stats) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'Hora');
-    data.addColumn('number', 'Acidentes');
-    data.addColumn({type: 'string', role: 'tooltip'});
-    data.addRows(_.map(stats, function(d) {
-      return [d.hora, d.qtd_ocorrencia, d.hora+"h: "+d.qtd_ocorrencia+" acidentes"]
-    }));
-    var options = { 
-          colors: ['#DF5A3E'],
-          width: '100%', height: '100%',
-          animation: { duration: 1000 },
-          backgroundColor: 'transparent',
-          chartArea:{width: '80%', height: '80%',},
-          curveType: 'function',
-          hAxis: { 
-                baselineColor: 'transparent',
-              ticks: [0,6,12,18,23], 
-              gridlines: { count: 4, color: 'transparent'}
-          },
-          vAxis:{ 
-             
-              textPosition :'none',
-               gridlines: { color: 'transparent'}
-          },
-          legend: { position: 'none'}                 
-        };
-          
-      var chart = new google.visualization.ColumnChart(document.getElementById('hour'));
-
-      chart.draw(data, options);
-  };
-  
   return self;
 });
