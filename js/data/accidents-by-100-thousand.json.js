958b97cd3ae4696df5ce10b88c6d583c15c79c5f
Ricardo Cavalcanti <rcavalca@thoughtworks.com>
2013-10-21 14:35:27 -0300

    incluindo dados separados por ano

---
+++ js/data/accidents-by-100-thousand.json.js
@@ -0,0 +1,974 @@
+[
+	{
+		'uf' : 'AC',
+		'ano' : 2007,
+		'rank' : 20,
+		'acidentes_por_100_mil' : 42
+	},
+	{
+		'uf' : 'AC',
+		'ano' : 2008,
+		'rank' : 20,
+		'acidentes_por_100_mil' : 45
+	},
+	{
+		'uf' : 'AC',
+		'ano' : 2009,
+		'rank' : 20,
+		'acidentes_por_100_mil' : 47
+	},
+	{
+		'uf' : 'AC',
+		'ano' : 2010,
+		'rank' : 19,
+		'acidentes_por_100_mil' : 60
+	},
+	{
+		'uf' : 'AC',
+		'ano' : 2011,
+		'rank' : 19,
+		'acidentes_por_100_mil' : 67
+	},
+	{
+		'uf' : 'AC',
+		'ano' : 2012,
+		'rank' : 19,
+		'acidentes_por_100_mil' : 64
+	},
+	{
+		'uf' : 'AL',
+		'ano' : 2007,
+		'rank' : 18,
+		'acidentes_por_100_mil' : 49
+	},
+	{
+		'uf' : 'AL',
+		'ano' : 2008,
+		'rank' : 18,
+		'acidentes_por_100_mil' : 54
+	},
+	{
+		'uf' : 'AL',
+		'ano' : 2009,
+		'rank' : 17,
+		'acidentes_por_100_mil' : 58
+	},
+	{
+		'uf' : 'AL',
+		'ano' : 2010,
+		'rank' : 16,
+		'acidentes_por_100_mil' : 69
+	},
+	{
+		'uf' : 'AL',
+		'ano' : 2011,
+		'rank' : 17,
+		'acidentes_por_100_mil' : 71
+	},
+	{
+		'uf' : 'AL',
+		'ano' : 2012,
+		'rank' : 18,
+		'acidentes_por_100_mil' : 65
+	},
+	{
+		'uf' : 'AM',
+		'ano' : 2007,
+		'rank' : 27,
+		'acidentes_por_100_mil' : 5
+	},
+	{
+		'uf' : 'AM',
+		'ano' : 2008,
+		'rank' : 27,
+		'acidentes_por_100_mil' : 5
+	},
+	{
+		'uf' : 'AM',
+		'ano' : 2009,
+		'rank' : 27,
+		'acidentes_por_100_mil' : 5
+	},
+	{
+		'uf' : 'AM',
+		'ano' : 2010,
+		'rank' : 27,
+		'acidentes_por_100_mil' : 6
+	},
+	{
+		'uf' : 'AM',
+		'ano' : 2011,
+		'rank' : 27,
+		'acidentes_por_100_mil' : 7
+	},
+	{
+		'uf' : 'AM',
+		'ano' : 2012,
+		'rank' : 27,
+		'acidentes_por_100_mil' : 5
+	},
+	{
+		'uf' : 'AP',
+		'ano' : 2007,
+		'rank' : 26,
+		'acidentes_por_100_mil' : 20
+	},
+	{
+		'uf' : 'AP',
+		'ano' : 2008,
+		'rank' : 26,
+		'acidentes_por_100_mil' : 23
+	},
+	{
+		'uf' : 'AP',
+		'ano' : 2009,
+		'rank' : 26,
+		'acidentes_por_100_mil' : 22
+	},
+	{
+		'uf' : 'AP',
+		'ano' : 2010,
+		'rank' : 26,
+		'acidentes_por_100_mil' : 22
+	},
+	{
+		'uf' : 'AP',
+		'ano' : 2011,
+		'rank' : 26,
+		'acidentes_por_100_mil' : 31
+	},
+	{
+		'uf' : 'AP',
+		'ano' : 2012,
+		'rank' : 26,
+		'acidentes_por_100_mil' : 31
+	},
+	{
+		'uf' : 'BA',
+		'ano' : 2007,
+		'rank' : 19,
+		'acidentes_por_100_mil' : 48
+	},
+	{
+		'uf' : 'BA',
+		'ano' : 2008,
+		'rank' : 19,
+		'acidentes_por_100_mil' : 51
+	},
+	{
+		'uf' : 'BA',
+		'ano' : 2009,
+		'rank' : 18,
+		'acidentes_por_100_mil' : 55
+	},
+	{
+		'uf' : 'BA',
+		'ano' : 2010,
+		'rank' : 18,
+		'acidentes_por_100_mil' : 63
+	},
+	{
+		'uf' : 'BA',
+		'ano' : 2011,
+		'rank' : 18,
+		'acidentes_por_100_mil' : 69
+	},
+	{
+		'uf' : 'BA',
+		'ano' : 2012,
+		'rank' : 17,
+		'acidentes_por_100_mil' : 67
+	},
+	{
+		'uf' : 'CE',
+		'ano' : 2007,
+		'rank' : 25,
+		'acidentes_por_100_mil' : 26
+	},
+	{
+		'uf' : 'CE',
+		'ano' : 2008,
+		'rank' : 24,
+		'acidentes_por_100_mil' : 31
+	},
+	{
+		'uf' : 'CE',
+		'ano' : 2009,
+		'rank' : 24,
+		'acidentes_por_100_mil' : 33
+	},
+	{
+		'uf' : 'CE',
+		'ano' : 2010,
+		'rank' : 23,
+		'acidentes_por_100_mil' : 43
+	},
+	{
+		'uf' : 'CE',
+		'ano' : 2011,
+		'rank' : 24,
+		'acidentes_por_100_mil' : 44
+	},
+	{
+		'uf' : 'CE',
+		'ano' : 2012,
+		'rank' : 24,
+		'acidentes_por_100_mil' : 42
+	},
+	{
+		'uf' : 'DF',
+		'ano' : 2007,
+		'rank' : 21,
+		'acidentes_por_100_mil' : 40
+	},
+	{
+		'uf' : 'DF',
+		'ano' : 2008,
+		'rank' : 22,
+		'acidentes_por_100_mil' : 37
+	},
+	{
+		'uf' : 'DF',
+		'ano' : 2009,
+		'rank' : 21,
+		'acidentes_por_100_mil' : 44
+	},
+	{
+		'uf' : 'DF',
+		'ano' : 2010,
+		'rank' : 22,
+		'acidentes_por_100_mil' : 46
+	},
+	{
+		'uf' : 'DF',
+		'ano' : 2011,
+		'rank' : 20,
+		'acidentes_por_100_mil' : 57
+	},
+	{
+		'uf' : 'DF',
+		'ano' : 2012,
+		'rank' : 20,
+		'acidentes_por_100_mil' : 51
+	},
+	{
+		'uf' : 'ES',
+		'ano' : 2007,
+		'rank' : 2,
+		'acidentes_por_100_mil' : 163
+	},
+	{
+		'uf' : 'ES',
+		'ano' : 2008,
+		'rank' : 2,
+		'acidentes_por_100_mil' : 166
+	},
+	{
+		'uf' : 'ES',
+		'ano' : 2009,
+		'rank' : 3,
+		'acidentes_por_100_mil' : 176
+	},
+	{
+		'uf' : 'ES',
+		'ano' : 2010,
+		'rank' : 2,
+		'acidentes_por_100_mil' : 204
+	},
+	{
+		'uf' : 'ES',
+		'ano' : 2011,
+		'rank' : 3,
+		'acidentes_por_100_mil' : 207
+	},
+	{
+		'uf' : 'ES',
+		'ano' : 2012,
+		'rank' : 3,
+		'acidentes_por_100_mil' : 198
+	},
+	{
+		'uf' : 'GO',
+		'ano' : 2007,
+		'rank' : 8,
+		'acidentes_por_100_mil' : 87
+	},
+	{
+		'uf' : 'GO',
+		'ano' : 2008,
+		'rank' : 7,
+		'acidentes_por_100_mil' : 100
+	},
+	{
+		'uf' : 'GO',
+		'ano' : 2009,
+		'rank' : 7,
+		'acidentes_por_100_mil' : 112
+	},
+	{
+		'uf' : 'GO',
+		'ano' : 2010,
+		'rank' : 8,
+		'acidentes_por_100_mil' : 125
+	},
+	{
+		'uf' : 'GO',
+		'ano' : 2011,
+		'rank' : 8,
+		'acidentes_por_100_mil' : 125
+	},
+	{
+		'uf' : 'GO',
+		'ano' : 2012,
+		'rank' : 9,
+		'acidentes_por_100_mil' : 118
+	},
+	{
+		'uf' : 'MA',
+		'ano' : 2007,
+		'rank' : 23,
+		'acidentes_por_100_mil' : 31
+	},
+	{
+		'uf' : 'MA',
+		'ano' : 2008,
+		'rank' : 23,
+		'acidentes_por_100_mil' : 34
+	},
+	{
+		'uf' : 'MA',
+		'ano' : 2009,
+		'rank' : 23,
+		'acidentes_por_100_mil' : 34
+	},
+	{
+		'uf' : 'MA',
+		'ano' : 2010,
+		'rank' : 24,
+		'acidentes_por_100_mil' : 38
+	},
+	{
+		'uf' : 'MA',
+		'ano' : 2011,
+		'rank' : 23,
+		'acidentes_por_100_mil' : 45
+	},
+	{
+		'uf' : 'MA',
+		'ano' : 2012,
+		'rank' : 23,
+		'acidentes_por_100_mil' : 44
+	},
+	{
+		'uf' : 'MG',
+		'ano' : 2007,
+		'rank' : 4,
+		'acidentes_por_100_mil' : 101
+	},
+	{
+		'uf' : 'MG',
+		'ano' : 2008,
+		'rank' : 4,
+		'acidentes_por_100_mil' : 110
+	},
+	{
+		'uf' : 'MG',
+		'ano' : 2009,
+		'rank' : 5,
+		'acidentes_por_100_mil' : 122
+	},
+	{
+		'uf' : 'MG',
+		'ano' : 2010,
+		'rank' : 6,
+		'acidentes_por_100_mil' : 133
+	},
+	{
+		'uf' : 'MG',
+		'ano' : 2011,
+		'rank' : 7,
+		'acidentes_por_100_mil' : 133
+	},
+	{
+		'uf' : 'MG',
+		'ano' : 2012,
+		'rank' : 7,
+		'acidentes_por_100_mil' : 128
+	},
+	{
+		'uf' : 'MS',
+		'ano' : 2007,
+		'rank' : 7,
+		'acidentes_por_100_mil' : 89
+	},
+	{
+		'uf' : 'MS',
+		'ano' : 2008,
+		'rank' : 5,
+		'acidentes_por_100_mil' : 102
+	},
+	{
+		'uf' : 'MS',
+		'ano' : 2009,
+		'rank' : 8,
+		'acidentes_por_100_mil' : 110
+	},
+	{
+		'uf' : 'MS',
+		'ano' : 2010,
+		'rank' : 5,
+		'acidentes_por_100_mil' : 136
+	},
+	{
+		'uf' : 'MS',
+		'ano' : 2011,
+		'rank' : 5,
+		'acidentes_por_100_mil' : 135
+	},
+	{
+		'uf' : 'MS',
+		'ano' : 2012,
+		'rank' : 6,
+		'acidentes_por_100_mil' : 128
+	},
+	{
+		'uf' : 'MT',
+		'ano' : 2007,
+		'rank' : 9,
+		'acidentes_por_100_mil' : 83
+	},
+	{
+		'uf' : 'MT',
+		'ano' : 2008,
+		'rank' : 8,
+		'acidentes_por_100_mil' : 98
+	},
+	{
+		'uf' : 'MT',
+		'ano' : 2009,
+		'rank' : 6,
+		'acidentes_por_100_mil' : 112
+	},
+	{
+		'uf' : 'MT',
+		'ano' : 2010,
+		'rank' : 9,
+		'acidentes_por_100_mil' : 118
+	},
+	{
+		'uf' : 'MT',
+		'ano' : 2011,
+		'rank' : 9,
+		'acidentes_por_100_mil' : 122
+	},
+	{
+		'uf' : 'MT',
+		'ano' : 2012,
+		'rank' : 5,
+		'acidentes_por_100_mil' : 133
+	},
+	{
+		'uf' : 'PA',
+		'ano' : 2007,
+		'rank' : 22,
+		'acidentes_por_100_mil' : 39
+	},
+	{
+		'uf' : 'PA',
+		'ano' : 2008,
+		'rank' : 21,
+		'acidentes_por_100_mil' : 41
+	},
+	{
+		'uf' : 'PA',
+		'ano' : 2009,
+		'rank' : 22,
+		'acidentes_por_100_mil' : 43
+	},
+	{
+		'uf' : 'PA',
+		'ano' : 2010,
+		'rank' : 20,
+		'acidentes_por_100_mil' : 54
+	},
+	{
+		'uf' : 'PA',
+		'ano' : 2011,
+		'rank' : 21,
+		'acidentes_por_100_mil' : 57
+	},
+	{
+		'uf' : 'PA',
+		'ano' : 2012,
+		'rank' : 22,
+		'acidentes_por_100_mil' : 48
+	},
+	{
+		'uf' : 'PB',
+		'ano' : 2007,
+		'rank' : 12,
+		'acidentes_por_100_mil' : 70
+	},
+	{
+		'uf' : 'PB',
+		'ano' : 2008,
+		'rank' : 10,
+		'acidentes_por_100_mil' : 85
+	},
+	{
+		'uf' : 'PB',
+		'ano' : 2009,
+		'rank' : 10,
+		'acidentes_por_100_mil' : 91
+	},
+	{
+		'uf' : 'PB',
+		'ano' : 2010,
+		'rank' : 12,
+		'acidentes_por_100_mil' : 99
+	},
+	{
+		'uf' : 'PB',
+		'ano' : 2011,
+		'rank' : 12,
+		'acidentes_por_100_mil' : 102
+	},
+	{
+		'uf' : 'PB',
+		'ano' : 2012,
+		'rank' : 11,
+		'acidentes_por_100_mil' : 103
+	},
+	{
+		'uf' : 'PE',
+		'ano' : 2007,
+		'rank' : 16,
+		'acidentes_por_100_mil' : 52
+	},
+	{
+		'uf' : 'PE',
+		'ano' : 2008,
+		'rank' : 15,
+		'acidentes_por_100_mil' : 58
+	},
+	{
+		'uf' : 'PE',
+		'ano' : 2009,
+		'rank' : 15,
+		'acidentes_por_100_mil' : 65
+	},
+	{
+		'uf' : 'PE',
+		'ano' : 2010,
+		'rank' : 14,
+		'acidentes_por_100_mil' : 80
+	},
+	{
+		'uf' : 'PE',
+		'ano' : 2011,
+		'rank' : 14,
+		'acidentes_por_100_mil' : 89
+	},
+	{
+		'uf' : 'PE',
+		'ano' : 2012,
+		'rank' : 15,
+		'acidentes_por_100_mil' : 77
+	},
+	{
+		'uf' : 'PI',
+		'ano' : 2007,
+		'rank' : 14,
+		'acidentes_por_100_mil' : 60
+	},
+	{
+		'uf' : 'PI',
+		'ano' : 2008,
+		'rank' : 13,
+		'acidentes_por_100_mil' : 64
+	},
+	{
+		'uf' : 'PI',
+		'ano' : 2009,
+		'rank' : 13,
+		'acidentes_por_100_mil' : 70
+	},
+	{
+		'uf' : 'PI',
+		'ano' : 2010,
+		'rank' : 13,
+		'acidentes_por_100_mil' : 83
+	},
+	{
+		'uf' : 'PI',
+		'ano' : 2011,
+		'rank' : 13,
+		'acidentes_por_100_mil' : 89
+	},
+	{
+		'uf' : 'PI',
+		'ano' : 2012,
+		'rank' : 13,
+		'acidentes_por_100_mil' : 85
+	},
+	{
+		'uf' : 'PR',
+		'ano' : 2007,
+		'rank' : 13,
+		'acidentes_por_100_mil' : 65
+	},
+	{
+		'uf' : 'PR',
+		'ano' : 2008,
+		'rank' : 9,
+		'acidentes_por_100_mil' : 88
+	},
+	{
+		'uf' : 'PR',
+		'ano' : 2009,
+		'rank' : 4,
+		'acidentes_por_100_mil' : 142
+	},
+	{
+		'uf' : 'PR',
+		'ano' : 2010,
+		'rank' : 4,
+		'acidentes_por_100_mil' : 191
+	},
+	{
+		'uf' : 'PR',
+		'ano' : 2011,
+		'rank' : 4,
+		'acidentes_por_100_mil' : 202
+	},
+	{
+		'uf' : 'PR',
+		'ano' : 2012,
+		'rank' : 4,
+		'acidentes_por_100_mil' : 186
+	},
+	{
+		'uf' : 'RJ',
+		'ano' : 2007,
+		'rank' : 11,
+		'acidentes_por_100_mil' : 72
+	},
+	{
+		'uf' : 'RJ',
+		'ano' : 2008,
+		'rank' : 11,
+		'acidentes_por_100_mil' : 82
+	},
+	{
+		'uf' : 'RJ',
+		'ano' : 2009,
+		'rank' : 12,
+		'acidentes_por_100_mil' : 87
+	},
+	{
+		'uf' : 'RJ',
+		'ano' : 2010,
+		'rank' : 11,
+		'acidentes_por_100_mil' : 100
+	},
+	{
+		'uf' : 'RJ',
+		'ano' : 2011,
+		'rank' : 10,
+		'acidentes_por_100_mil' : 112
+	},
+	{
+		'uf' : 'RJ',
+		'ano' : 2012,
+		'rank' : 10,
+		'acidentes_por_100_mil' : 105
+	},
+	{
+		'uf' : 'RN',
+		'ano' : 2007,
+		'rank' : 10,
+		'acidentes_por_100_mil' : 82
+	},
+	{
+		'uf' : 'RN',
+		'ano' : 2008,
+		'rank' : 12,
+		'acidentes_por_100_mil' : 77
+	},
+	{
+		'uf' : 'RN',
+		'ano' : 2009,
+		'rank' : 11,
+		'acidentes_por_100_mil' : 89
+	},
+	{
+		'uf' : 'RN',
+		'ano' : 2010,
+		'rank' : 10,
+		'acidentes_por_100_mil' : 105
+	},
+	{
+		'uf' : 'RN',
+		'ano' : 2011,
+		'rank' : 11,
+		'acidentes_por_100_mil' : 109
+	},
+	{
+		'uf' : 'RN',
+		'ano' : 2012,
+		'rank' : 12,
+		'acidentes_por_100_mil' : 102
+	},
+	{
+		'uf' : 'RO',
+		'ano' : 2007,
+		'rank' : 3,
+		'acidentes_por_100_mil' : 121
+	},
+	{
+		'uf' : 'RO',
+		'ano' : 2008,
+		'rank' : 3,
+		'acidentes_por_100_mil' : 141
+	},
+	{
+		'uf' : 'RO',
+		'ano' : 2009,
+		'rank' : 2,
+		'acidentes_por_100_mil' : 179
+	},
+	{
+		'uf' : 'RO',
+		'ano' : 2010,
+		'rank' : 3,
+		'acidentes_por_100_mil' : 198
+	},
+	{
+		'uf' : 'RO',
+		'ano' : 2011,
+		'rank' : 2,
+		'acidentes_por_100_mil' : 222
+	},
+	{
+		'uf' : 'RO',
+		'ano' : 2012,
+		'rank' : 2,
+		'acidentes_por_100_mil' : 204
+	},
+	{
+		'uf' : 'RR',
+		'ano' : 2007,
+		'rank' : 6,
+		'acidentes_por_100_mil' : 94
+	},
+	{
+		'uf' : 'RR',
+		'ano' : 2008,
+		'rank' : 14,
+		'acidentes_por_100_mil' : 64
+	},
+	{
+		'uf' : 'RR',
+		'ano' : 2009,
+		'rank' : 16,
+		'acidentes_por_100_mil' : 59
+	},
+	{
+		'uf' : 'RR',
+		'ano' : 2010,
+		'rank' : 21,
+		'acidentes_por_100_mil' : 47
+	},
+	{
+		'uf' : 'RR',
+		'ano' : 2011,
+		'rank' : 22,
+		'acidentes_por_100_mil' : 56
+	},
+	{
+		'uf' : 'RR',
+		'ano' : 2012,
+		'rank' : 21,
+		'acidentes_por_100_mil' : 49
+	},
+	{
+		'uf' : 'RS',
+		'ano' : 2007,
+		'rank' : 5,
+		'acidentes_por_100_mil' : 99
+	},
+	{
+		'uf' : 'RS',
+		'ano' : 2008,
+		'rank' : 6,
+		'acidentes_por_100_mil' : 101
+	},
+	{
+		'uf' : 'RS',
+		'ano' : 2009,
+		'rank' : 9,
+		'acidentes_por_100_mil' : 105
+	},
+	{
+		'uf' : 'RS',
+		'ano' : 2010,
+		'rank' : 7,
+		'acidentes_por_100_mil' : 130
+	},
+	{
+		'uf' : 'RS',
+		'ano' : 2011,
+		'rank' : 6,
+		'acidentes_por_100_mil' : 134
+	},
+	{
+		'uf' : 'RS',
+		'ano' : 2012,
+		'rank' : 8,
+		'acidentes_por_100_mil' : 125
+	},
+	{
+		'uf' : 'SC',
+		'ano' : 2007,
+		'rank' : 1,
+		'acidentes_por_100_mil' : 220
+	},
+	{
+		'uf' : 'SC',
+		'ano' : 2008,
+		'rank' : 1,
+		'acidentes_por_100_mil' : 235
+	},
+	{
+		'uf' : 'SC',
+		'ano' : 2009,
+		'rank' : 1,
+		'acidentes_por_100_mil' : 270
+	},
+	{
+		'uf' : 'SC',
+		'ano' : 2010,
+		'rank' : 1,
+		'acidentes_por_100_mil' : 293
+	},
+	{
+		'uf' : 'SC',
+		'ano' : 2011,
+		'rank' : 1,
+		'acidentes_por_100_mil' : 290
+	},
+	{
+		'uf' : 'SC',
+		'ano' : 2012,
+		'rank' : 1,
+		'acidentes_por_100_mil' : 271
+	},
+	{
+		'uf' : 'SE',
+		'ano' : 2007,
+		'rank' : 15,
+		'acidentes_por_100_mil' : 53
+	},
+	{
+		'uf' : 'SE',
+		'ano' : 2008,
+		'rank' : 17,
+		'acidentes_por_100_mil' : 56
+	},
+	{
+		'uf' : 'SE',
+		'ano' : 2009,
+		'rank' : 19,
+		'acidentes_por_100_mil' : 55
+	},
+	{
+		'uf' : 'SE',
+		'ano' : 2010,
+		'rank' : 17,
+		'acidentes_por_100_mil' : 66
+	},
+	{
+		'uf' : 'SE',
+		'ano' : 2011,
+		'rank' : 15,
+		'acidentes_por_100_mil' : 75
+	},
+	{
+		'uf' : 'SE',
+		'ano' : 2012,
+		'rank' : 16,
+		'acidentes_por_100_mil' : 75
+	},
+	{
+		'uf' : 'SP',
+		'ano' : 2007,
+		'rank' : 24,
+		'acidentes_por_100_mil' : 26
+	},
+	{
+		'uf' : 'SP',
+		'ano' : 2008,
+		'rank' : 25,
+		'acidentes_por_100_mil' : 27
+	},
+	{
+		'uf' : 'SP',
+		'ano' : 2009,
+		'rank' : 25,
+		'acidentes_por_100_mil' : 29
+	},
+	{
+		'uf' : 'SP',
+		'ano' : 2010,
+		'rank' : 25,
+		'acidentes_por_100_mil' : 30
+	},
+	{
+		'uf' : 'SP',
+		'ano' : 2011,
+		'rank' : 25,
+		'acidentes_por_100_mil' : 32
+	},
+	{
+		'uf' : 'SP',
+		'ano' : 2012,
+		'rank' : 25,
+		'acidentes_por_100_mil' : 32
+	},
+	{
+		'uf' : 'TO',
+		'ano' : 2007,
+		'rank' : 17,
+		'acidentes_por_100_mil' : 49
+	},
+	{
+		'uf' : 'TO',
+		'ano' : 2008,
+		'rank' : 16,
+		'acidentes_por_100_mil' : 56
+	},
+	{
+		'uf' : 'TO',
+		'ano' : 2009,
+		'rank' : 14,
+		'acidentes_por_100_mil' : 66
+	},
+	{
+		'uf' : 'TO',
+		'ano' : 2010,
+		'rank' : 15,
+		'acidentes_por_100_mil' : 72
+	},
+	{
+		'uf' : 'TO',
+		'ano' : 2011,
+		'rank' : 16,
+		'acidentes_por_100_mil' : 74
+	},
+	{
+		'uf' : 'TO',
+		'ano' : 2012,
+		'rank' : 14,
+		'acidentes_por_100_mil' : 78
+	}
+]
