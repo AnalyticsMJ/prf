034ac63a3932b24e082ba691c858cecf57eb9343
Ricardo Cavalcanti <rcavalca@thoughtworks.com>
2013-10-24 17:59:09 -0300

    alinhando bottom dos divs

--- js/app/models/corners.js
+++ js/app/models/corners.js
@@ -1,8 +1,8 @@
 define([], function() {
   return {
-  	"topleft":     {top:"1.3em", left:"1.3em", bottom: "auto",  right: "auto"},
-  	"bottomleft":  {top:"auto",  left:"1.3em", bottom: "1.3em", right: "auto"},
-  	"topright":    {top:"1.3em", left:"auto",  bottom: "auto",  right: "1.3em"},
-  	"bottomright": {top:"auto",  left:"auto",  bottom: "1.3em", right: "1.3em"}
+  	"topleft":     {top:"0", left:"1.3em", bottom: "auto",  right: "auto"},
+  	"bottomleft":  {top:"auto",  left:"1.3em", bottom: "0", right: "auto"},
+  	"topright":    {top:"0", left:"auto",  bottom: "auto",  right: "1.3em"},
+  	"bottomright": {top:"auto",  left:"auto",  bottom: "0", right: "1.3em"}
   };
 });
