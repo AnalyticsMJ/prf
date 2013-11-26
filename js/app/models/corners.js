b3026193262a3415c29f51d3af67be41771834c0
Ricardo Cavalcanti <rcavalca@thoughtworks.com>
2013-11-26 16:21:30 -0300

    nome do estado em cima do mapa para ficar mais visivel

--- js/app/models/corners.js
+++ js/app/models/corners.js
@@ -1,6 +1,6 @@
 define([], function() {
   return {
-  	"topleft":     {top:"0", left:"1.3em", bottom: "auto",  right: "auto"},
+  	"topleft":     {top:"3em", left:"1.3em", bottom: "auto",  right: "auto"},
   	"bottomleft":  {top:"auto",  left:"1.3em", bottom: "0", right: "auto"},
   	"topright":    {top:"0", left:"auto",  bottom: "auto",  right: "1.3em"},
   	"bottomright": {top:"auto",  left:"auto",  bottom: "0", right: "1.3em"}
