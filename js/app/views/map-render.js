2da5ad431bc96af515f54e7f3464531221452097
Ricardo Cavalcanti <rcavalca@thoughtworks.com>
2013-11-28 18:32:21 -0300

    omitindo rodovias estaduais

--- js/app/views/map-render.js
+++ js/app/views/map-render.js
@@ -1,58 +1,61 @@
 define(['kartograph', 'qtip', 'app/models/corners', 'app/views/thumbnail-view', 'chroma'], function($K, qtip, corners, thumbnail) {
-    return function () {
-      var map = $K.map('#mapa'); 
+  return function() {
+    var map = $K.map('#mapa');
 
-      // initialize qtip tooltip class
-      $.fn.qtip.defaults.style.classes = 'ui-tooltip-bootstrap';
-      
-      this.showMapOf = function (state, year) {
-        map.clear();
-        $('#legenda').css(corners.get(state.corner));
- 
-        thumbnail.render(state)
-        map.loadCSS('css/map.css', function() {
-          var mapPath = 'img/estados/'+ state.abbreviation +'.svg';
-          map.loadMap(mapPath, function() {
-            renderingOfMap(year); 
-          });
+    // initialize qtip tooltip class
+    $.fn.qtip.defaults.style.classes = 'ui-tooltip-bootstrap';
+
+    this.showMapOf = function(state, year) {
+      map.clear();
+      $('#legenda').css(corners.get(state.corner));
+
+      thumbnail.render(state)
+      map.loadCSS('css/map.css', function() {
+        var mapPath = 'img/estados/' + state.abbreviation + '.svg';
+        map.loadMap(mapPath, function() {
+          renderingOfMap(year);
         });
-
-      };
-
-      this.changeYear = function(year){
-        map.getLayer('rodovias').remove();
-        addRodovias(year);
-      }
-
-      function getQuantity(data, year){
-        var qtd = data['qtd-acidentes-'+year] || 0;
-        if (qtd === 'NULL') qtd = 0;
-
-        var km = data['km-final'] - data['km-inicial']
-        return  (qtd/km).toFixed(2);
-      }
-
-      function renderingOfMap(year) {
-        map.addLayer('estado');
-        addRodovias(year);
-      }
-
-      function addRodovias(year){
-        map.addLayer('rodovias',{
-          tooltips: function(d) {
-            var title = d.rodovia + ' (km' + d['km-inicial'] + ' - km' + d['km-final'] + ')',
-              details = getQuantity(d, year).toString() + ' acidentes por km';
-            return [title, details];
-          }
-        });
-        
-        var colorscale = new chroma.scale('Reds').domain([0, .5, 1, 5, 10, 25, 50]);
-
-        map.getLayer('rodovias').style('stroke', function(data) {
-          return colorscale(getQuantity(data, year));
-        });
-        map.fadeIn();
-      }
+      });
 
     };
+
+    this.changeYear = function(year) {
+      map.getLayer('rodovias').remove();
+      addRodovias(year);
+    }
+
+    function getQuantity(data, year) {
+      var qtd = data['qtd-acidentes-' + year] || 0;
+      if (qtd === 'NULL') qtd = 0;
+
+      var km = data['km-final'] - data['km-inicial']
+      return (qtd / km).toFixed(2);
+    }
+
+    function renderingOfMap(year) {
+      map.addLayer('estado');
+      addRodovias(year);
+    }
+
+    function addRodovias(year) {
+      map.addLayer('rodovias', {
+        filter: function(d) {
+          return d.rodovia.indexOf('BR') == 0;
+        },
+        tooltips: function(d) {
+          var title = d.rodovia + ' (km' + d['km-inicial'] + ' - km' + d['km-final'] + ')',
+            details = getQuantity(d, year).toString() + ' acidentes por km';
+          return [title, details];
+        }
+      });
+
+      var colorscale = new chroma.scale('Reds').domain([0, .5, 1, 5, 10, 25, 50]);
+
+      map.getLayer('rodovias').style('stroke', function(data) {
+        return colorscale(getQuantity(data, year));
+      });
+      map.fadeIn();
+    }
+
+  };
 });
